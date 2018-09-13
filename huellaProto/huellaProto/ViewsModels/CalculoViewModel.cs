53fc02648b60389cd152aa78327abf5652d5cc93
elkrlos10 <www.kliche10@gmail.com>
2018-09-13 13:13:54 -0500

    Calculo

--- huellaProto/huellaProto/ViewsModels/CalculoViewModel.cs
+++ huellaProto/huellaProto/ViewsModels/CalculoViewModel.cs
@@ -1,10 +1,76 @@
-﻿using System;
+﻿using huellaProto.Service;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace huellaProto.ViewModels
 {
-    public class CalculoViewModel
+    public class CalculoViewModel: BaseViewModel
     {
+        #region Service
+        private ApiService apiService;
+        #endregion
+
+        #region atributos
+        private double toneladasCo2;
+        private string region;
+        private string arbol;
+        private bool precision;
+        #endregion
+
+        #region Propiedades
+
+        public double ToneladasCo2
+        {
+            get { return this.toneladasCo2; }
+            set { SetValue(ref this.toneladasCo2, value); }
+        }
+        public string Region
+        {
+            get { return this.region; }
+            set { SetValue(ref this.region, value); }
+        }
+        public string Arbol
+        {
+            get { return this.arbol; }
+            set { SetValue(ref this.arbol, value); }
+        }
+        public bool Precision
+        {
+            get { return this.precision; }
+            set { SetValue(ref this.precision, value); }
+        }
+        #endregion
+
+        #region Constructor
+        public CalculoViewModel()
+        {
+            this.apiService = new ApiService();
+            this.CalculoHuella();
+        }
+
+        #endregion
+
+
+        #region Metodos
+        private async void CalculoHuella()
+        {
+            try
+            {
+                var response = await this.apiService.Post<double>(
+                                 MainViewModel.GetInstance().UrlServices,
+                                 "api/Proyecto",
+                                "/RegristarVehiculos", MainViewModel.GetInstance().IdProyecto);
+
+                var cosa = response.Result;
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+
+        }
+        #endregion
+
     }
 }
